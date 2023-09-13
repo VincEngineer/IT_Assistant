af2d0abe6607606571e5541dd94ec258477f74c6
Vincenzo Bonaiuto <105960233+VincEngineer@users.noreply.github.com>
2023-09-13 01:24:01 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
-# install.sh
+
+# Function to print text in green
+print_green() {
+    printf "\e[32m$1\e[0m\n"
+}
+
+# Function to print text in red
+print_red() {
+    printf "\e[31m$1\e[0m\n"
+}
+
+# Check if the script is running as root
+if [ "$EUID" -ne 0 ]; then
+    print_red "Please run as root"
+    exit 1
+else
+    print_green "Running as root"
+fi
 
 # Update package list and install dependencies
-sudo apt-get update -y
-sudo apt-get install -y '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev
-sudo apt-get install -y dbus-x11
-sudo apt-get install -y terminator
-sudo apt-get install -y sshpass
-sudo apt-get install -y python3-pip git
+apt-get update -y
+apt-get install -y '^libxcb.*-dev' libx11-xcb-dev libglu1-mesa-dev libxrender-dev libxi-dev
+apt-get install -y dbus-x11 terminator sshpass python3-pip git
 
-# Get the current username
-CURRENT_USER=$(whoami)
+# Get the current username (not effective username because of sudo)
+CURRENT_USER=$(who am i | awk '{print $1}')
 
 # Get the current directory
 CURRENT_DIR=$(pwd)
@@ -27,45 +41,43 @@ Exec=/usr/bin/python3 ${CURRENT_DIR}/Main.py
 Icon=${CURRENT_DIR}/Kali_Linux_Red.ico
 Terminal=false
 Type=Application
-Categories=Utility;" | sudo tee "/usr/share/applications/IT_Assistant.desktop"
+Categories=Utility;" > "/usr/share/applications/IT_Assistant.desktop"
 
 # Update desktop database
-sudo update-desktop-database
+update-desktop-database
 
-# Python dependencies
-pip install --user --upgrade pip
-pip install --user colorama
-pip install --user PyQt6
-pip install --user configparser
-pip install --user requests
-
-
-
+# Switch to the regular user for pip installation
+sudo -u ${CURRENT_USER} pip install --upgrade pip
+sudo -u ${CURRENT_USER} pip install colorama PyQt6 configparser requests
 
 # Check for Desktop Entry
 if [ -e "/usr/share/applications/IT_Assistant.desktop" ]; then
-    echo "Desktop Entry exists."
+    print_green "Desktop Entry exists."
 else
-    echo "Desktop Entry does not exist."
+    print_red "Desktop Entry does not exist."
     exit 1
 fi
 
 # Check for Desktop directory
 if [ -d "/home/${CURRENT_USER}/Desktop/" ]; then
-    echo "Desktop directory exists."
+    print_green "Desktop directory exists."
     if [ -w "/home/${CURRENT_USER}/Desktop/" ]; then
-        echo "Desktop directory is writable."
+        print_green "Desktop directory is writable."
     else
-        echo "Desktop directory is not writable."
+        print_red "Desktop directory is not writable."
         exit 1
     fi
 else
-    echo "Desktop directory does not exist."
+    print_red "Desktop directory does not exist."
     exit 1
 fi
 
 # Create the symlink
 ln -s "/usr/share/applications/IT_Assistant.desktop" "/home/${CURRENT_USER}/Desktop/IT_Assistant.desktop"
 
-
-
+if [ $? -eq 0 ]; then
+    print_green "Symlink created successfully."
+else
+    print_red "Failed to create symlink."
+    exit 1
+fi
