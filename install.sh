ab19566bd5d7efec3b30a691abdec50075822818
Vincenzo Bonaiuto <105960233+VincEngineer@users.noreply.github.com>
2023-09-13 01:28:52 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -81,3 +81,11 @@ else
     print_red "Failed to create symlink."
     exit 1
 fi
+
+exit
+# Python dependencies
+pip install --user --upgrade pip
+pip install --user colorama
+pip install --user PyQt6
+pip install --user configparser
+pip install --user requests
